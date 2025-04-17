d5db20039c0f412bbadbafa3e8d6294f898d8576
Laura Gagliazzo <lauragag19@gmail.com>
2025-04-17 14:09:38 +0200

    Completato selfwork components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,14 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Selfwork-rotte-viste</title>
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    
-    <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
